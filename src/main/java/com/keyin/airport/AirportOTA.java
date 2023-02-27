b9a66558c801d56459d759505e5a0942afdcbe49
kbalsom <kara.balsom@keyin.com>
2023-02-27 16:49:35 -0330

    Fixed airports bug

--- src/main/java/com/keyin/airport/AirportOTA.java
+++ src/main/java/com/keyin/airport/AirportOTA.java
@@ -2,42 +2,42 @@ package com.keyin.airport;
 
 public class AirportOTA {
 
-    private String airportName;
-    private String airportCode;
+        private String name;
+        private String code;
 
-    private long cityId;
+        private long cityId;
 
-    private long aircraftId;
+        private long aircraftId;
 
-    public String getName() {
-        return airportName;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public long getCityId() {
+            return cityId;
+        }
+
+        public void setCityId(long cityId) {
+            this.cityId = cityId;
+        }
+
+        public long getAircraftId() {
+            return aircraftId;
+        }
+
+        public void setAircraftId(long aircraftId) {
+            this.aircraftId = aircraftId;
+        }
     }
-
-    public void setName(String airportName) {
-        this.airportName = airportName;
-    }
-
-    public String getCode() {
-        return airportCode;
-    }
-
-    public void setCode(String code) {
-        this.airportCode = airportCode;
-    }
-
-    public long getCityId() {
-        return cityId;
-    }
-
-    public void setCityId(long cityId) {
-        this.cityId = cityId;
-    }
-
-    public long getAircraftId() {
-        return aircraftId;
-    }
-
-    public void setAircraftId(long aircraftId) {
-        this.aircraftId = aircraftId;
-    }
-}
