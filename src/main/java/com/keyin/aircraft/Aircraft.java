2dace6debeccfa6d627cb142ee3a247a44cfe3b7
David Turner <davidbrockturner@hotmail.com>
2023-03-03 11:04:34 -0330

    Comments added to Airport.java

--- src/main/java/com/keyin/aircraft/Aircraft.java
+++ src/main/java/com/keyin/aircraft/Aircraft.java
@@ -6,19 +6,22 @@ import com.keyin.airport.Airport;
 import javax.persistence.*;
 import java.util.List;
 
+// Class Aircraft is an entity class and is mapped to the table aircraft in the database
 @Entity
 public class Aircraft {
-
+    // @Id annotation is used to specify the primary key of an entity and @SequenceGenerator is used to generate the primary key value automatically
+    // @GeneratedValue is used to specify the type of generation strategy is used to generate the primary key value
     @Id
     @SequenceGenerator(name = "aircraft_sequence", sequenceName = "aircraft_sequence", allocationSize = 1, initialValue=1)
     @GeneratedValue(generator = "aircraft_sequence")
     private long id;
-
+    // instance variables of the class Aircraft
     private String type;
     private String airlineName;
     private int numberOfPassengers;
 
-
+    // relationships between the entity classes Aircraft and Airport and Aircraft and Passenger
+    // NOTE: Why is there no toString() method for the relationship between Aircraft, Airport and Passenger?
     @ManyToMany
     private List<Airport> airports;
 
@@ -27,7 +30,7 @@ public class Aircraft {
 
     public Aircraft() {
     }
-
+    // Below are the getters and setters for the instance variables of the class Aircraft
     public long getId() {
         return id;
     }
--- src/main/java/com/keyin/airport/AirportController.java
+++ src/main/java/com/keyin/airport/AirportController.java
@@ -1,31 +1,41 @@
 package com.keyin.airport;
 
+// AirportController.java this is a controller class and is used to handle the HTTP requests and responses
 
+// aircraft.Aircraft is used to import the Aircraft class, aircraft.AircraftRepository is used to import the AircraftRepository interface
+// this is similar for city.City and city.CityRepository
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.keyin.aircraft.Aircraft;
 import com.keyin.aircraft.AircraftRepository;
 import com.keyin.city.City;
 import com.keyin.city.CityRepository;
+// net.minidev.json.JSONObject is used to import the JSONObject class from the json-smart library
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+// the imports below are used to import the classes and interfaces that are used in the class AirportController
+// without the javax.servlet.http.HttpServletResponse import, the HttpServletResponse response parameter in the updateAirport method will not work
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+// @RestController annotation is used to create RESTful web services using Spring MVC
+// @CrossOrigin annotation is used to allow cross-origin requests
+// cross-origin requests are requests that are made from a different domain than the domain of the resource being requested
 @RestController
 @CrossOrigin
 public class AirportController {
+    // @Autowired annotation is used to inject the AirportRepository dependency into the AirportController class
+    // private AirportRepository repo creates a new instance of the AirportRepository class ( AirportRepository repo = new AirportRepository() )
+    // this is similar for the CityRepository and AircraftRepository classes
     @Autowired
     private AirportRepository repo;
 
     @Autowired
     private CityRepository cityRepo;
-
 
     @Autowired
     private AircraftRepository aircraftRepo;
@@ -35,17 +45,20 @@ public class AirportController {
         return (List<Airport>) repo.findAll();
     }
 
-
+    // @GetMapping annotation is used to handle the HTTP GET requests matched with the given URI expression
+    // @GetMapping("/airport/{id}") will get the airport with the id that is passed in the URI
     @GetMapping("/airport/{id}")
     public Optional<Airport> getAirportById(@PathVariable Long id) {
         return repo.findById(id);
     }
 
+    // @GetMapping("/airport/passengers_search") will get the airport with the passenger's last name if it matches the last name that is passed in the URI
     @GetMapping("/airport/passengers_search")
     public List<Airport> getAirportByPassengers(@RequestParam String lastName) {
         return (List<Airport>) repo.findByPassengers_lastName(lastName);
     }
 
+    // @GetMapping("/airport_passengers") the purpose of this is to answer the question: "What airports have passengers?"
     @GetMapping("/airports_passengers")
     private List<JSONObject> getAllAirportsByPassengers() {
         List <Airport> airportList = (List<Airport>) repo.findAll();
@@ -66,6 +79,7 @@ public class AirportController {
         return arrayList;
     }
 
+    // @PostMapping("/airport") will create a new airport with the data that is passed in the request body
     @PostMapping("/airport")
     public void createAirport(@RequestBody AirportOTA airportOTA) {
 
@@ -88,7 +102,7 @@ public class AirportController {
 
     }
 
-
+    //  @PutMapping("/airport/{id}") will update the airport with the id that is passed in the URI with the data that is passed in the request body
     @PutMapping("/airport/{id}")
     public void updateAirport(@PathVariable String id, @RequestBody Airport airport, HttpServletResponse response) {
         Optional<Airport> returnValue = repo.findById(Long.parseLong(id));
@@ -110,6 +124,7 @@ public class AirportController {
         }
     }
 
+    // @DeleteMapping("/airport/{id}") will delete the airport with the id that is passed in the URI
     @DeleteMapping("/airport/{id}")
     public void deleteAirport(@PathVariable String id, HttpServletResponse response) {
         Optional<Airport> returnValue = repo.findById(Long.parseLong(id));
