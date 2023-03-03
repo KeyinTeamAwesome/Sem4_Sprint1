fce7f30c094206a3f5c230cf6a43077cfea24461
David Turner <davidbrockturner@hotmail.com>
2023-03-02 21:08:06 -0330

    Comments added to Airport.java

--- src/main/java/com/keyin/airport/Airport.java
+++ src/main/java/com/keyin/airport/Airport.java
@@ -15,18 +15,20 @@ public class Airport {
     @Id
     @SequenceGenerator(name = "airport_sequence", sequenceName = "airport_sequence", allocationSize = 1, initialValue = 1)
     @GeneratedValue(generator = "airport_sequence")
+    // private long id, String airportName, String airportCode are instance variables of the class Airport
     private long id;
     private String airportName;
     private String airportCode;
 
+    // @ManyToMany annotation is used to specify the many-to-many relationship between the entity classes Airport and Passenger
     @ManyToMany
     private List<Passenger> passengers;
 
-
+    // public Airport() is a default constructor and has to have no arguments passed to it
     public Airport() {
     }
 
-
+    // Below are the getters and setters for the instance variables of the class Airport
     public long getId() {
         return id;
     }
@@ -55,6 +57,7 @@ public class Airport {
         return (List<Passenger>) passengers;
     }
 
+    // tpString() method is used to return the airportName of the airport
     public String toString(){
         return this.airportName;
     }
